edb5ed58aa92698d04c352a7641025639f9c7c96
Valentin <contact@valentin-cosson.fr>
2021-12-08 10:43:50 +0100

    Add missing fixtures (for importing data)

---
+++ jellysmack_test/fixtures/__init__.py
@@ -0,0 +1,6 @@
+import os
+
+BASE_PATH = os.path.dirname(os.path.realpath(__file__))
+
+CHARACTERS_PATH = f"{BASE_PATH}/rick_morty-characters_v1.json"
+EPISODES_PATH = f"{BASE_PATH}/rick_morty-episodes_v1.json"
